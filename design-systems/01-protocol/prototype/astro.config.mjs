621e916227210033ff6bee6ec60b04319d8946e1
Black <black@BlackdeMacBook-Air.local>
2026-07-26 09:38:47 +0800

    feat(seo): use vercel server-side redirects instead of astro static html redirects

--- design-systems/01-protocol/prototype/astro.config.mjs
+++ design-systems/01-protocol/prototype/astro.config.mjs
@@ -16,13 +16,6 @@ export default defineConfig({
       filter: (page) => !page.endsWith('/gallery/') && !page.endsWith('/join/'),
     }),
   ],
-  redirects: {
-    '/join/': '/about/#join',
-    '/archives/': '/articles/',
-    '/archives/[id]': '/articles/[id]',
-    '/en/archives/': '/en/articles/',
-    '/en/archives/[id]': '/en/articles/[id]',
-  },
   build: {
     format: 'directory',
   },
